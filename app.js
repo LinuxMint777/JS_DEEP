4eddab08939e17cf6de212dd1a175836cc58d1e4
LinuxMint777 <57522042+LinuxMint777@users.noreply.github.com>
2024-10-16 03:46:22 +0000

    npm pacakage and nodemon install

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello world')
+class Person{
+    constructor(name, email){
+        this._name = name
+        this._email = email
+    }
+
+    get name(){
+        return this._name.toUpperCase()
+    }
+    set name(value){
+        this._name = value
+    }
+}
+
+let p1 = new Person('Jahid Hasan', 'gsvmch777@gmail.com')
+p1.name = 'linuxmint'
+console.log(p1.name)
